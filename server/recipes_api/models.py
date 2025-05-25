742558fc34b99b22e56a925e57e2d4caf626f87f
Dmitriy Safronov <dimasafronov55@mail.ru>
2025-05-25 16:15:46 +0300

    Started project, greated models

---
+++ server/recipes_api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Dish(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    cooking_time = models.PositiveIntegerField()  # в минутах
