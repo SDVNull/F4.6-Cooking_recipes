bd62cd84e33648bd8dc36949889a5648d9a2103b
Dmitriy Safronov <dimasafronov55@mail.ru>
2025-05-25 16:37:07 +0300

    created simple serializers, views, added spectacular settings

--- server/recipes_api/views.py
+++ server/recipes_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Category, Dish
+from .serializers import CategorySerializer, DishSerializer
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class DishViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
